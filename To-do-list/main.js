4bbb6c35371bce26e17e7bb4e0efd25d95a1585c
İdil Bağubek <135833150+idilbagubek@users.noreply.github.com>
2023-09-12 08:38:30 -0700

    Update main.js

--- To-do-list/main.js
+++ To-do-list/main.js
@@ -2,13 +2,6 @@
 let done = document.getElementsByClassName("done-icon");
 let input = document.querySelectorAll(".input");
 let remove = document.querySelectorAll(".remove-icon");
-let icon = document.querySelectorAll(".fa-regular fa-circle")
-
-
-
-console.log(done.length);
-console.log(done.forEach);
-console.log(input[0].textContent)
 
 for(let i = 0; i < done.length; i++) {
    done[i].addEventListener("click" , function(){
